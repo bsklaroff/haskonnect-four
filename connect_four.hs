3b84408064f17f014bc8711cf69693d6de5fa4b6
Ben Sklaroff <bsklaroff@gmail.com>
2014-03-09 03:59:21 -0700

    finish checking win conditions

--- connect_four.hs
+++ connect_four.hs
@@ -3,6 +3,7 @@ import System.IO
 data Piece = X | O deriving (Show, Eq)
 data Col = Col [Piece] deriving (Eq)
 data Board = Board {width :: Int, height :: Int, cols :: [Col]} deriving (Eq)
+data Dir = Up | Right | RightUp | RightDown deriving (Eq)
 
 otherPiece :: Piece -> Piece
 otherPiece p = if p == X then O else X
@@ -24,20 +25,42 @@ instance Show Board where
     show (Board w h c) = concatMap (f h) c ++ "\n" ++ show (Board w (h-1) c)
       where f h (Col xs) = if h <= length xs then show (xs !! (h-1)) else "-"
 
+gameOver :: Board -> (Bool, Piece)
+gameOver (Board _ _ []) = (False, X)
+gameOver b@(Board w h ((Col c):cs)) =
+    let res = gameOver' b in
+      if fst res then res else gameOver (Board (w-1) h cs)
+    where gameOver' b@(Board w h ((Col c):cs)) = foldl (cw b c) (False, X) [(x, y) | x <- [0..length c - 1], y <- [Up, Main.Right, RightUp, RightDown]]
+          cw b c res@(rb, _) (i, d) = if rb then res else checkWin b i d (c !! i) 4
+
+checkWin :: Board -> Int -> Dir -> Piece -> Int -> (Bool, Piece)
+checkWin (Board _ _ []) _ _ p _ = (False, p)
+checkWin b@(Board w h ((Col c):cs)) r d p k
+  | r < 0 || r >= length c = (False, p)
+  | c !! r /= p = (False, p)
+  | k == 1 = (True, p)
+  | d == Up = checkWin b (r+1) d p (k-1)
+  | d == Main.Right = checkWin (Board (w-1) h cs) r d p (k-1)
+  | d == RightUp = checkWin (Board (w-1) h cs) (r+1) d p (k-1)
+  | d == RightDown = checkWin (Board (w-1) h cs) (r-1) d p (k-1)
+
 gameLoop :: Piece -> Board -> IO ()
 gameLoop p b = do
     putStr ((show p) ++ " to play. Enter a number between 1 and " ++ (show $ width b) ++ ": ")
     hFlush stdout
     col <- getLine
     case reads col of
-      [(intCol, "")] -> let newB = boardPlay p b intCol in do
+      [(intCol, "")] -> do
+        let newB = boardPlay p b intCol
         putStr $ show newB
         if newB == b then do
                      putStrLn "Illegal Move"
                      gameLoop p newB
                      else do
                        putStrLn ((show p) ++ " dropped in column " ++ col)
-                       gameLoop (otherPiece p) newB
+                       let gameRes = gameOver newB
+                       if fst gameRes then putStrLn ((show . snd $ gameRes) ++ " won!")
+                                      else gameLoop (otherPiece p) newB
       _ -> do putStrLn "Please enter a valid integer"
               gameLoop p b
 
